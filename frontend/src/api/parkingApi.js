5c3e263e1ba76c79b93e6be51ffe047dfc2a9ad6
Vedan8 <vedanjyadav@gmail.com>
2026-02-26 18:13:40 +0530

    route

--- frontend/src/api/parkingApi.js
+++ frontend/src/api/parkingApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL; // your DRF API endpoint
 
-export const getSlots = () => axios.get(`${API_URL}slots/`);
-export const addSlot = (slot) => axios.post(`${API_URL}slots/`, slot);
-export const parkVehicle = (data) => axios.post(`${API_URL}park/`, data);
-export const removeVehicle = (id) => axios.post(`${API_URL}remove/${id}/`);
+export const getSlots = () => axios.get(`${API_URL}/slots/`);
+export const addSlot = (slot) => axios.post(`${API_URL}/slots/`, slot);
+export const parkVehicle = (data) => axios.post(`${API_URL}/park/`, data);
+export const removeVehicle = (id) => axios.post(`${API_URL}/remove/${id}/`);
